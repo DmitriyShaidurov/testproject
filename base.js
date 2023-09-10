40bc1e25c9ededc05d4f8cd8401930f81f4d1a20
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2023-09-10 19:45:05 +0500

    База

--- base.js
+++ base.js
@@ -1,1 +1,27 @@
-console.log("test");
+const div = document.createElement("div");
+const header = document.createElement("h1");
+header.innerText = "Learn Js";
+const a = document.createElement("a");
+a.href = "https://developer.mozilla.org/ru/docs/Learn";
+a.innerText = "MDN";
+header.classList.add("test");
+
+div.appendChild(header);
+div.appendChild(a);
+document.body.appendChild(div);
+
+console.log(div);
+console.log(header);
+
+const aTag = document.querySelector("a");
+console.log(aTag);
+aTag.addEventListener("click", function (e) {
+  e.preventDefault();
+  /* window.location = "https://vk.com";
+  console.log("test"); */
+  test();
+});
+
+function test() {
+  console.log(div);
+}
